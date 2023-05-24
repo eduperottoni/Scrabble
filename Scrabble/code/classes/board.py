2b9dd5625af15afb8d7417bfd429d74876dfecaf
Eduardo Perottoni <edu.perottoni@gmail.com>
2023-05-24 01:55:43 -0300

    Creating algorithms modeling diagrams

--- Scrabble/code/classes/board.py
+++ Scrabble/code/classes/board.py
@@ -6,10 +6,20 @@ class Board:
     def __init__(self):
         self.__bag = Bag(CARDS_QUANTITY_BY_LETTER)
         self.__dictionary = Dictionary()
+        self.__current_word = []
     
     @property
     def bag(self):
         return self.__bag
+
+    @property
+    def current_word(self):
+        return self.__current_word
     
+    def calculate_player_score(self):
+        word = self.__current_word
+        word_multiply_const = 1
+        pass
+
     
     
